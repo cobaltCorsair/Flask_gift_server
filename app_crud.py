a35cc70fc1b81fda068a7a883281cf9038929915
cobaltCorsair <cobaltcorsair@yandex.ru>
2022-01-27 20:52:28 +0300

    add crud operation

--- app_crud.py
+++ app_crud.py
@@ -1,37 +1,24 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from app import db
 from sqlalchemy.exc import IntegrityError
-# импортируем классы Book и Base из файла database_setup.py
 from models import Username, Presents, UserPresents
-
-engine = create_engine('sqlite:///user_presents.db')
-# Свяжем engine с метаданными класса Base,
-# чтобы декларативы могли получить доступ через экземпляр DBSession
-Base.metadata.bind = engine
-
-DBSession = sessionmaker(bind=engine)
-# Экземпляр DBSession() отвечает за все обращения к базе данных
-# и представляет «промежуточную зону» для всех объектов,
-# загруженных в объект сессии базы данных.
-session = DBSession()
 
 try:
     user = Username(name="Moderator", forum_id='2')
     # Чтобы сохранить наш объект user, мы добавляем его в сессию:
-    session.add(user)
+    db.session.add(user)
     # Чтобы сохранить изменения в нашу базу данных и зафиксировать
     # транзакцию, используем commit(). Любое изменение,
     # внесенное для объектов в сессии, не будет сохранено
     # в базу данных, пока не будет вызван session.commit().
-    session.commit()
+    db.session.commit()
     print('Добавлено в бд')
 except IntegrityError:
     # откат в случае ошибки (неуникальный id)
-    session.rollback()
+    db.session.rollback()
     print('Такая запись уже есть')
 else:
     # логика добавления другой записи, если id уникален
     pass
 
 
-print(session.query(Username).all())
+print(db.session.query(Username).all())
