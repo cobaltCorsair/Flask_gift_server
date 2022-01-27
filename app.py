4f1d1d1569d1431b3cb9c58a8110bd19e70fa207
cobaltCorsair <cobaltcorsair@yandex.ru>
2022-01-27 23:30:32 +0300

    add crud operation

--- app.py
+++ app.py
@@ -7,15 +7,17 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 cors = CORS(app)  # включаем заголовки для кроссбраузерного ответа
 
+from app_crud import add_new_user
 
 @app.route('/getandpost', methods=["POST"])
 @cross_origin()
 def get_post_javascript_data():
     data = request.data
-    print(data)
+    if data is not '':
+        print(data)
+        add_new_user('Test', '3')
     return 'Какой-то ответ серверу'
 
-import models
 
 if __name__ == "__main__":
     app.run('0.0.0.0', 5000)  # запустим сервер на 5000 порту
--- app_crud.py
+++ app_crud.py
@@ -1,24 +1,26 @@
-from app import db
+from models import Username, db
 from sqlalchemy.exc import IntegrityError
-from models import Username, Presents, UserPresents
 
-try:
-    user = Username(name="Moderator", forum_id='2')
-    # Чтобы сохранить наш объект user, мы добавляем его в сессию:
-    db.session.add(user)
-    # Чтобы сохранить изменения в нашу базу данных и зафиксировать
-    # транзакцию, используем commit(). Любое изменение,
-    # внесенное для объектов в сессии, не будет сохранено
-    # в базу данных, пока не будет вызван session.commit().
-    db.session.commit()
-    print('Добавлено в бд')
-except IntegrityError:
-    # откат в случае ошибки (неуникальный id)
-    db.session.rollback()
-    print('Такая запись уже есть')
-else:
-    # логика добавления другой записи, если id уникален
-    pass
+
+def add_new_user(person_name, id):
+    try:
+        user = Username(name=person_name, forum_id=id)
+        # Чтобы сохранить наш объект user, мы добавляем его в сессию:
+        db.session.add(user)
+        # Чтобы сохранить изменения в нашу базу данных и зафиксировать
+        # транзакцию, используем commit(). Любое изменение,
+        # внесенное для объектов в сессии, не будет сохранено
+        # в базу данных, пока не будет вызван session.commit().
+        db.session.commit()
+        print('Добавлено в бд')
+
+    except IntegrityError:
+        # откат в случае ошибки (неуникальный id)
+        db.session.rollback()
+        print('Такая запись уже есть')
+    else:
+        # логика добавления другой записи, если id уникален
+        pass
 
 
 print(db.session.query(Username).all())
