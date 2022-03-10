eec9e4e7284f721fdc477840cc078d5518189895
cobaltCorsair <cobaltcorsair@yandex.ru>
2022-03-10 16:19:04 +0300

    Added page style

--- page.js
+++ page.js
@@ -1,23 +1,26 @@
-<div class="Basket"></div>
+<div className="Basket"></div>
 
 <script type="text/javascript">
-(async () => {
-  let response = await fetch('http://127.0.0.1:5000/getallpresents', {
-  method: 'POST',
-  headers: {
+    (async () => {
+    let response = await fetch('http://127.0.0.1:5000/getallpresents', {
+    method: 'POST',
+    headers: {
     'Content-Type': 'application/json'
-  }
+}
 });
     let result = await response.json();
     let data = JSON.parse(JSON.stringify(result));
     let out = '';
     for(let key in data) {
-    out+='<div id="present"><h3>'+data[key]['id']+'</h3>';
-    out+='<p>'+data[key]['name']+'</p>';
-    out+='<p>'+data[key]['title']+'</p>';
-    out+='<img src="'+data[key]['image']+'"></div>'; // Формируем URL картинки
+    out+='<div class="item" data-id="'+data[key]['id']+'"';
+    out+='data-name="'+data[key]['name']+'"';
+    out+='data-desc="'+data[key]['title']+'"';
+    out+='<div class="item_image"><div class="item_image"><div class="item_container"><img src="'+data[key]['image']+'" width="41" height="40"></div></div>';
+    out+='<template id="'+data[key]['id']+'>';
+    out+='<div class="item-desc">'+data[key]['title']+'</div></template></div>';
 };
 
-$('.Basket').html(out);
-})();
+    $('.Basket').html(out);
+})
+();
 </script>
